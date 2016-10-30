853831b1174c4fee0ac462ab9411c24a850f24f5
Teresa <TC807496@wcupa.edu>
2016-10-30 15:29:54 -0400

    Add the memorycard script and the scenecontroller, still need to add the matching, score, etc.

---
+++ Assets/scripts/SceneController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneController : MonoBehaviour {
+	public const int gridRows = 2;
+	public const int gridCols = 4;
+	public const float offsetX = 2f;
+	public const float offsetY = 2.5f;
+
+	[SerializeField] private MemoryCard originalCard;
+	[SerializeField] private Sprite[] images;
+
+	private MemoryCard _firstRevealed;
+	private MemoryCard _secondRevealed;
+
+	// Use this for initialization
+	void Start () {
+		Vector3 startPos = originalCard.transform.position;
+
+		for (int i = 0; i < gridCols; i++) {
+			for (int j = 0; j < gridRows; j++) {
+				MemoryCard card;
+
+				// use the original for the first grid space
+				if (i == 0 && j == 0) {
+					card = originalCard;
+				} else {
+					card = Instantiate (originalCard) as MemoryCard;
+				}
+
+				int id = Random.Range(0, images.Length);
+				card.SetCard (id, images [id]);
+
+				float posX = (offsetX * i) + startPos.x;
+				float posY = -(offsetY * j) + startPos.y;
+				card.transform.position = new Vector3 (posX, posY, startPos.z);
+	
+			}
+		}
+	}
+	private int[] ShuffleArray(int[] numbers) {
+		int[] newArray = numbers.Clone() as int[];
+		for (int i = 0; i < newArray.Length; i++ ) {
+			int tmp = newArray[i];
+			int r = Random.Range(i, newArray.Length);
+			newArray[i] = newArray[r];
+			newArray[r] = tmp;
+		}
+		return newArray;
+	}
+
+	public bool canReveal {
+		get {return _secondRevealed == null;}
+	}
+
+	public void CardRevealed(MemoryCard card) {
+		if (_firstRevealed == null) {
+			_firstRevealed = card;
+		} else {
+			_secondRevealed = card;
+			Debug.Log("Match? " + (_firstRevealed.id == _secondRevealed.id));
+		}
+	}
+	
+
+}
